cbf498c1903fe96c15958b832bec591e2f0c2a26
Farhod Akramov <akramovfrank@gmail.com>
2023-04-19 22:04:43 -0600

    inqurier prompts are done time to work on fs

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+import inquirer from "inquirer";
+
+function askQuestions() {}
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Type up to 3 [LETTERS/NUMBERS] to be displayed on the Logo",
+      name: "characters",
+    },
+    //3 Characters COLOR
+    {
+      type: "input",
+      message: "Choose a color for the Characters. Color Keyword or Hex Number",
+      name: "charactersColor",
+    },
+    //Shape Selection
+    {
+      type: "list",
+      message: "Choose a Shape for your Logo",
+      choices: ["Circle", "Triangle", "Square"],
+      name: "shape",
+    },
+    //Shape Color
+    {
+      type: "input",
+      message: "Choose a color for the Shape. Color Keyword or Hex Number",
+      name: "shapeColor",
+    },
+  ])
+  .then((answers) => {
+    if (answers.characters.length > 3) {
+      console.log("Your logo can't have more than 3 characters");
+      askQuestions();
+    } else {
+      //use answers to generate the LOGO
+      generateLogo("logo.svg", answers);
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.log(error);
+    } else {
+      // Something else went wrong
+      console.log("This thing is broken");
+    }
+  });
+
+askQuestions();
